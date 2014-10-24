9f434b65b5d1a8971f2d17fc74b6789a862135be
z313430421 <bestboy6@163.com>
2014-10-24 10:45:36 +0800

    win

--- src/com/example/surfaceviewtest/SurfaceViewTest.java
+++ src/com/example/surfaceviewtest/SurfaceViewTest.java
@@ -1,15 +1,85 @@
 package com.example.surfaceviewtest;
 
+import java.util.Random;
+
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SurfaceViewTest extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_surface_view_test);
-    }
+	FastRenderView renderView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		renderView = new FastRenderView(this);
+		setContentView(renderView);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		renderView.resume();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		renderView.pause();
+	}
+
+	class FastRenderView extends SurfaceView implements Runnable {
+
+		Thread renderThread = null;
+		SurfaceHolder holder;
+		volatile boolean running = false;
+		Random random = new Random(255);
+
+		public FastRenderView(Context context) {
+			super(context);
+			holder = getHolder();
+		}
+
+		@Override
+		public void run() {
+			while (running) {
+				if (!holder.getSurface().isValid()) {
+					continue;
+				}
+				Canvas canvas = holder.lockCanvas();
+				canvas.drawRGB(random.nextInt(), random.nextInt(), random.nextInt());
+				holder.unlockCanvasAndPost(canvas);
+			}
+		}
+
+		private void resume() {
+			running = true;
+			renderThread = new Thread(this);
+			renderThread.start();
+		}
+
+		private void pause() {
+			running = false;
+			while (true) {
+				try {
+					renderThread.join();
+					return;
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
